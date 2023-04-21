59b90c045c413dcb9c02bc6bedfbe9c33c5ecc4e
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-04-21 05:12:01 +0300

    Add card style.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,59 @@ import DortmundLogo from "./images/logo_bvb.jpg";
 import DortmundHeader from "./images/header_bvb.jpg";
 
 function App(){
+
+    const twitterArsenal="https://twitter.com/Arsenal";
+    const twitterRealMadrid="https://twitter.com/realmadrid";
+    const twitterDortmund="https://twitter.com/BVB";
+
+    const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris."
+
     return (
         <div>
-            <div>Football Clubs</div>
-            <ProfileCard title="Arsenal FC" handle="@Arsenal" logoImage={ArsenalLogo} headerImage={ArsenalHeader} />
-            <ProfileCard title="Real Madrid CF" handle="@realmadrid" logoImage={RealMadridLogo} headerImage={ReadlMadridHeader} />
-            <ProfileCard title="Borussia Dortmund" handle="@BVB" logoImage={DortmundLogo} headerImage={DortmundHeader} />
+        <section class="hero is-danger">
+            <div class="hero-body">
+                <p class="title">Football Clubs</p>
+                <p class="subtitle">MyTeams</p>
+            </div>
+        </section>
+
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Arsenal FC" 
+                                handle="@Arsenal" 
+                                logoImage={ArsenalLogo} 
+                                headerImage={ArsenalHeader} 
+                                twitterAdress={twitterArsenal}
+                                description={description} 
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Real Madrid CF" 
+                                handle="@realmadrid" 
+                                logoImage={RealMadridLogo} 
+                                headerImage={ReadlMadridHeader} 
+                                twitterAdress={twitterRealMadrid}
+                                description={description} 
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Borussia Dortmund" 
+                                handle="@BVB" 
+                                logoImage={DortmundLogo} 
+                                headerImage={DortmundHeader} 
+                                twitterAdress={twitterDortmund}
+                                description={description} 
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
         </div>
     )
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,17 +1,34 @@
-function ProfileCard({title, handle, logoImage, headerImage}){
+function ProfileCard({title, handle, logoImage, headerImage, twitterAdress, description}){
 
-//Aşağıdaki 3 satır ile yukarıdaki function tanımına eklediğimiz {title, handle} aynı işi yapar.
-// const title = props.title;
-// const handle = props.handle;
+    //Aşağıdaki 3 satır ile yukarıdaki function tanımına eklediğimiz {title, handle} aynı işi yapar.
+    // const title = props.title;
+    // const handle = props.handle;
 
-//const {title, handle} = props;
-
-
+    //const {title, handle} = props;
+    
     return (
-        <div>
-            <img src={logoImage} alt="FC Logo"/>
-            <div>Title : {title}</div> 
-            <div>Handle : {handle}</div>
+        <div className="card">
+            <div className="card-image">
+            <figure className="image is-4by3">
+                <img src={headerImage} alt="FC Header"/>
+            </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-left">
+                        <figure className="image is-48x48">
+                            <img src={logoImage} alt="FC Logo"/>
+                        </figure>
+                    </div>
+                    <div className="media-content">
+                        <p className="title is-4">{title}</p>
+                        <p className="subtitle is-6"><a href={twitterAdress} target="_blank" rel="noreferrer">{handle}</a></p>
+                    </div>
+                </div>
+
+                <div className="content">{description}</div>
+            </div>
         </div>
     )
 }
